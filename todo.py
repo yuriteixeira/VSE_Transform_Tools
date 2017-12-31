5a888a38c0988c172129b04aebb9e4461b9a30ca
doakey3 <github.dmodo@spamgourmet.com>
2017-12-31 16:51:49 -0700

    removed junk file

--- todo.py
+++
@@ -1,9 +0,0 @@
-* create new properties delta and gamma scales.
-
-  - scaling with the addon changes delta scale
-  - scaling with the crop tool changes the gamma scale
-  - the overall scale is linked to a driver that is a combination of gamma and delta scale
-
-* Enable cropping and canvas collapse on meta strips
-
-  - make a reciprocal function that gets the overall height and width
