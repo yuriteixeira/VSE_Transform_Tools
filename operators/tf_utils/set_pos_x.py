b81e34143f6a5ff86c824b6a240d0c11114d11bd
doakey3 <github.dmodo@spamgourmet.com>
2018-01-15 12:37:49 -0700

    making tag for older version

--- operators/tf_utils/set_pos_x.py
+++
@@ -1,8 +0,0 @@
-import bpy
-
-def set_pos_x(strip, pos):
-    '''Set the X position of a strip'''
-    res_x = bpy.context.scene.render.resolution_x
-    if strip.translation_unit == 'PERCENT':
-        pos = pos * 100 / res_x
-    return pos 
