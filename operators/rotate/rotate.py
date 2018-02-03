fix minor bugs with rotation

operators/rotate/rotate.py
@@ -15,6 +15,9 @@ from ..utils.selection import ensure_transforms
 
 from ..utils.draw import draw_px_point
 
+from .get_rotation_change import get_rotation_change
+from .apply_strip_rotation import apply_strip_rotation
+
 
 class Rotate(bpy.types.Operator):
     """
@@ -33,19 +36,17 @@ class Rotate(bpy.types.Operator):
     center_area = Vector([0, 0])
     center_real = Vector([0, 0])
 
+    rot_prev = 0
     vec_init = Vector([0, 0])
+    vec_prev = Vector([0, 0])
     vec_act = Vector([0, 0])
 
     key_val = ''
     key_period = False
     key_period_val = 1
 
-    rot_marker = 0
-    revolutions = 0
-
+    stepwise_increment = 5
     slow_factor = 10
-    pre_slow_rot = 0
-    post_slow_rot = 0
 
     @classmethod
     def poll(cls, context):
@@ -59,59 +60,25 @@ class Rotate(bpy.types.Operator):
         context.area.tag_redraw()
 
         if self.tab:
-            self.vec_act = Vector(
-                (event.mouse_region_x, event.mouse_region_y))
+            self.vec_act = Vector([event.mouse_region_x, event.mouse_region_y])
             self.vec_act -= self.center_area
 
-            rot = math.degrees(
-                -self.vec_init.angle_signed(self.vec_act))
-            rot += self.post_slow_rot
+            rot = math.degrees(-self.vec_prev.angle_signed(self.vec_act))
 
-            if rot < 0 and self.rot_marker > 0:
-                rot = 360 + rot
-            elif rot > 0 and self.rot_marker < 0:
-                rot = rot - 360
-
-            if self.rot_marker < rot and self.rot_marker - rot <= -180:
-                self.revolutions -= 1
-            elif self.rot_marker > rot and self.rot_marker - rot >= 180:
-                self.revolutions += 1
-
-            self.rot_marker = rot
-
-            if 'SHIFT' in event.type and event.value == 'PRESS':
-                self.pre_slow_rot = rot + (self.revolutions * 360)
-
-            elif 'SHIFT' in event.type and event.value == 'RELEASE':
-                rev_degs = self.revolutions * 360
-                slow_fac = (self.slow_factor - 1) / self.slow_factor
-
-                self.post_slow_rot += self.pre_slow_rot - rot - rev_degs
-                self.post_slow_rot *= slow_fac
-
-                rot = rev_degs + (rot - self.pre_slow_rot)
+            if event.shift:
                 rot /= self.slow_factor
-                rot += self.pre_slow_rot
 
-                self.pre_slow_rot = 0
+            self.rot_prev += rot
+            self.rot_prev %= 360
 
-            elif event.shift:
-                rev_degs = self.revolutions * 360
+            self.vec_prev = Vector(self.vec_act)
 
-                rot = rev_degs + (rot - self.pre_slow_rot)
-                rot /= self.slow_factor
-                rot += self.pre_slow_rot
-
-            rot = rot % 360
-            if rot < -180:
-                rot = rot + 360
-            elif rot > 180:
-                rot = rot - 360
+            rot = float(self.rot_prev)
 
             process_input(self, event.type, event.value)
             if self.key_val != '':
                 try:
-                    rot = float(self.key_val)
+                    rot = -float(self.key_val)
                 except ValueError:
                     pass
 
@@ -120,89 +87,12 @@ class Rotate(bpy.types.Operator):
                 init_rot = self.tab_init[i]
                 init_t = self.tab_init_t[i]
 
-                if init_rot < -180:
-                    init_rot = 360 + init_rot
-                if init_rot > 180:
-                    init_rot = -360 + init_rot
+                apply_strip_rotation(
+                    self, strip, rot, init_rot, init_t, event)
 
-                flip_x = 1
-                if strip.use_flip_x:
-                    flip_x = -1
-
-                flip_y = 1
-                if strip.use_flip_y:
-                    flip_y = -1
-
-                strip_rot = init_rot + flip_x * flip_y * rot
-
-                rotation_increment = 5
-                if event.ctrl:
-                    strip_rot = int(strip_rot / rotation_increment)
-                    strip_rot *= rotation_increment
-
-                pivot_type = context.scene.seq_pivot_type
-
-                if (pivot_type == '1' or
-                   pivot_type in ['0', '3'] and len(self.tab) == 1):
-                    strip.rotation_start = strip_rot
-
-                elif pivot_type in ['0', '3'] and len(self.tab) > 1:
-                    pos_init = Vector([init_t[0], init_t[1]])
-
-                    pos_flip_x = flip_x * self.center_real.x
-                    pos_flip_y = flip_y * self.center_real.y
-                    pos_flip = Vector([pos_flip_x, pos_flip_y])
-
-                    pos_init -= pos_flip
-                    point_rot = flip_x * flip_y * math.radians(rot)
-
-                    np = rotate_point(pos_init, point_rot)
-                    if event.ctrl:
-                        p_rot_degs = math.radians(strip_rot - init_rot)
-                        point_rot = flip_x * flip_y * p_rot_degs
-                        np = rotate_point(pos_init, point_rot)
-
-                    pos_x = np.x + flip_x * self.center_real.x
-                    pos_x = set_pos_x(strip, pos_x)
-
-                    pos_y = np.y + flip_y * self.center_real.y
-                    pos_y = set_pos_y(strip, pos_y)
-
-                    if np.x == 0 and np.y == 0:
-                        strip.rotation_start = strip_rot
-
-                    else:
-                        strip.rotation_start = strip_rot
-                        strip.translate_start_x = pos_x
-                        strip.translate_start_y = pos_y
-
-                elif pivot_type == '2':
-                    fac = get_res_factor()
-
-                    pos_x = flip_x * context.scene.seq_cursor2d_loc[0]
-                    pos_y = flip_y * context.scene.seq_cursor2d_loc[1]
-
-                    center_c2d = Vector([pos_x, pos_y])
-                    center_c2d /= fac
-
-                    pos_init = Vector([init_t[0], init_t[1]])
-                    pos_init -= center_c2d
-
-                    point_rot = flip_x * flip_y * math.radians(rot)
-
-                    np = rotate_point(pos_init, point_rot)
-                    if event.ctrl:
-                        p_rot_degs = math.radians(strip_rot - init_rot)
-                        point_rot = flip_x * flip_y * p_rot_degs
-                        np = rotate_point(pos_init, point_rot)
-
-                    strip.rotation_start = strip_rot
-
-                    pos_x = set_pos_x(strip, np.x + center_c2d.x)
-                    pos_y = set_pos_y(strip, np.y + center_c2d.y)
-
-                    strip.translate_start_x = pos_x
-                    strip.translate_start_y = pos_y
+            if event.ctrl:
+                rot = int(rot / self.stepwise_increment)
+                rot *= self.stepwise_increment
 
             info_rot = (rot)
             context.area.header_text_set("Rotation %.4f " % info_rot)
@@ -268,7 +158,9 @@ class Rotate(bpy.types.Operator):
             self.center_real = Vector([0, 0])
             self.center_area = Vector([0, 0])
             self.key_val = ''
+            self.slow_additions = []
             rotated_count = 0
+
 
             self.tab = ensure_transforms()
 
@@ -337,7 +229,10 @@ class Rotate(bpy.types.Operator):
 
                 self.vec_init = Vector(
                     (event.mouse_region_x, event.mouse_region_y))
+
                 self.vec_init -= self.center_area
+
+                self.vec_prev = Vector(self.vec_init)
 
             args = (self, context)
             self.handle_line = bpy.types.SpaceSequenceEditor.draw_handler_add(