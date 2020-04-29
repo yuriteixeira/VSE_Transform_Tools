1f08d0e64c4e266d077f53a3fc15aa1444b4057c
tin2tin <tintwotin@gmail.com>
2020-04-29 08:19:37 +0200

    Add polls and checks.

--- __init__.py
+++ __init__.py
@@ -212,18 +212,20 @@ class vse_transform_tools_grab(WorkSpaceTool):
 
     @classmethod
     def poll(cls, context):
-        if context.scene.sequence_editor:
-            return True
-        return False
+        if context.scene and context.scene.sequence_editor and context.scene.sequence_editor.active_strip:
+            return context.scene.sequence_editor.active_strip.type == 'TRANSFORM'
+        else:
+            return False
 
     def draw_settings(context, layout, tool):
         scene = context.scene
         strip = scene.sequence_editor.active_strip
-        tool.operator_properties("vse_transform_tools.grab")
-        layout.prop(strip, "interpolation")
-        layout.prop(strip, "translation_unit")
-        layout.prop(strip, "translate_start_x", text="X")
-        layout.prop(strip, "translate_start_y", text="Y")
+        if scene and strip and strip.type == 'TRANSFORM':
+            tool.operator_properties("vse_transform_tools.grab")
+            layout.prop(strip, "interpolation")
+            layout.prop(strip, "translation_unit")
+            layout.prop(strip, "translate_start_x", text="X")
+            layout.prop(strip, "translate_start_y", text="Y")
 
 
 class vse_transform_tools_rotate(WorkSpaceTool):
@@ -243,11 +245,19 @@ class vse_transform_tools_rotate(WorkSpaceTool):
          {"properties": []}),
     )
 
+    @classmethod
+    def poll(cls, context):
+        if context.scene and context.scene.sequence_editor and context.scene.sequence_editor.active_strip:
+            return context.scene.sequence_editor.active_strip.type == 'TRANSFORM'
+        else:
+            return False
+
     def draw_settings(context, layout, tool):
         tool.operator_properties("vse_transform_tools.rotate")
         scene = context.scene
         strip = scene.sequence_editor.active_strip
-        layout.prop(strip, "rotation_start", text="Rotation")
+        if scene and strip and strip.type == 'TRANSFORM':
+            layout.prop(strip, "rotation_start", text="Rotation")
 
 
 class vse_transform_tools_scale(WorkSpaceTool):
@@ -267,14 +277,22 @@ class vse_transform_tools_scale(WorkSpaceTool):
          {"properties": []}),
     )
 
+    @classmethod
+    def poll(cls, context):
+        if context.scene and context.scene.sequence_editor and context.scene.sequence_editor.active_strip:
+            return context.scene.sequence_editor.active_strip.type == 'TRANSFORM'
+        else:
+            return False
+
     def draw_settings(context, layout, tool):
         tool.operator_properties("vse_transform_tools.scale")
         scene = context.scene
         strip = scene.sequence_editor.active_strip
-        layout.prop(strip, "interpolation")
-        layout.prop(strip, "translation_unit")
-        layout.prop(strip, "scale_start_x", text="X")
-        layout.prop(strip, "scale_start_y", text="Y")
+        if scene and strip and strip.type == 'TRANSFORM':
+            layout.prop(strip, "interpolation")
+            layout.prop(strip, "translation_unit")
+            layout.prop(strip, "scale_start_x", text="X")
+            layout.prop(strip, "scale_start_y", text="Y")
 
 
 class vse_transform_tools_crop(WorkSpaceTool):
@@ -294,11 +312,19 @@ class vse_transform_tools_crop(WorkSpaceTool):
          {"properties": []}),
     )
 
+    @classmethod
+    def poll(cls, context):
+        if context.scene and context.scene.sequence_editor and context.scene.sequence_editor.active_strip:
+            return context.scene.sequence_editor.active_strip.type == 'TRANSFORM'
+        else:
+            return False
+
     def draw_settings(context, layout, tool):
         tool.operator_properties("vse_transform_tools.crop")
         scene = context.scene
         strip = scene.sequence_editor.active_strip
-        layout.label(text=strip.name)
+        if scene and strip and strip.type == 'TRANSFORM':
+            layout.label(text=strip.name)
 
 
 class SEQUENCER_PT_track_transform_ui(bpy.types.Panel):
