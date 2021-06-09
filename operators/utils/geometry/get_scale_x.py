e2752225e145a5ed76f77a6796fd61ce75c483d7
Olivier Jolly <olivier@pcedev.com>
2021-06-09 23:34:37 +0200

    Add support for 3.x scale operation while retaining 2.92+ compatibility

--- operators/utils/geometry/get_scale_x.py
+++ operators/utils/geometry/get_scale_x.py
@@ -3,5 +3,5 @@ def get_scale_x(strip):
     Get X Scale for strip
     """
     if strip.type == "TRANSFORM":
-        return strip.start_scale_x
+        return getattr(strip, "start_scale_x", getattr(strip, "scale_start_x"))
     return strip.transform.scale_x
--- operators/utils/geometry/get_scale_y.py
+++ operators/utils/geometry/get_scale_y.py
@@ -3,5 +3,5 @@ def get_scale_y(strip):
     Get Y Scale for strip
     """
     if strip.type == "TRANSFORM":
-        return strip.start_scale_y
+        return getattr(strip, "start_scale_y", getattr(strip, "scale_start_y"))
     return strip.transform.scale_y
