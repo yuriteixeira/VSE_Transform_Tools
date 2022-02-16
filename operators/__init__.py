bb75c688e08dd93a11995903069104130a6a436a
Yuri Teixeira <oyuriteixeira@gmail.com>
2022-02-17 00:18:52 +0100

    feat: remove all other features (but crop, ofc)

--- operators/__init__.py
+++ operators/__init__.py
@@ -1,12 +1,2 @@
-from .adjust_alpha import PREV_OT_adjust_alpha
 from .autocrop import PREV_OT_autocrop
-from .call_menu import PREV_MT_menu_insert_keyframe, PREV_OT_call_menu, PREV_OT_insert_keyframe
 from .crop import PREV_OT_crop
-from .delete import PREV_OT_delete
-from .duplicate import PREV_OT_duplicate
-from .group import PREV_OT_group
-from .meta_toggle import PREV_OT_meta_toggle
-from .mouse_track import PREV_OT_mouse_track
-from .pixelate import PREV_OT_pixelate
-from .set_cursor2d import PREV_OT_set_cursor_2d
-from .track_transform import SEQUENCER_OT_track_transform
