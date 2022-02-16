d8fa80c4344e455ff6e47c7915546df682fa41ed
Yuri Teixeira <oyuriteixeira@gmail.com>
2022-02-17 00:39:55 +0100

    chore: metadata updates

--- __init__.py
+++ __init__.py
@@ -5,11 +5,11 @@ from bpy.utils import register_class
 from bpy.utils import unregister_class
 
 bl_info = {
-    "name": "VSE Transform Tools",
-    "description": "Quickly manipulate video strips in Blender's Video Sequence Editor",
-    "author": "kgeogeo, DoubleZ, doakey3, NathanLovato, zeograd",
-    "version": (1, 2, 9),
-    "blender": (2, 92, 0),
+    "name": "Video Strip Crop (VSC)",
+    "description": "Crop video strips in Video Sequence Editor (VSE)",
+    "author": "yuriteixeira",
+    "version": (0, 0, 1),
+    "blender": (3, 0, 0),
     "wiki_url": "https://github.com/doakey3/VSE_Transform_Tools",
     "tracker_url": "https://github.com/doakey3/VSE_Transform_Tools/issues",
     "category": "Sequencer"
